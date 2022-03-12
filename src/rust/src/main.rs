290d2fb01e037f093662f33dd080e9b4da031a53
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-03-12 21:21:18 +0100

    Add rust version

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -6,56 +6,64 @@ fn main() {
     let suffix_device_path = "/dev";
     let raw_device_path = format!("{suffix_device_path}/{raw_device}");
     let partition_device_path =  format!("{suffix_device_path}/{partition_device}");
-    let file_system_path = "/mnt";
+    let file_system_path = "/media/usb";
+    let start_usb = !true;
 
+    // https://serverfault.com/questions/338937/differences-between-dev-sda-and-dev-sda1
     println!("## Summary");
-    println!("Raw device: {}", raw_device);
-    println!("Raw device path: {}", raw_device_path);
-    println!("Partition device: {}", partition_device);
-    println!("Partition device path: {}", partition_device_path);
-    println!("File system path: {}", file_system_path);
+    println!();
+    println!("- Raw device: {}", raw_device);
+    println!("- Raw device path: {}", raw_device_path);
+    println!("- Partition device: {}", partition_device);
+    println!("- Partition device path: {}", partition_device_path);
+    println!("- File system path: {}", file_system_path);
     println!();
 
 
     println!("## System current status");
     println!();
-    println!("### Devices status");
-    run_command(&format!("ls {raw_device_path}*"));
+    print_devices_status(&raw_device, &suffix_device_path);
     println!();
     print_mount_status(&raw_device);
 
-    run_mount(&partition_device_path, &file_system_path, &raw_device);
-    //println!("- Mount");
-    //println!("{}", String::from_utf8_lossy(&output.stdout));
-
-    //let output = Command::new("bash")
-    //        .arg("-c")
-    //        .arg(format!("ls /dev | grep {raw_device}"))
-    //        .output()
-    //        .expect("failed to execute process");
-    //println!("## Devices");
-    //println!("{}", String::from_utf8_lossy(&output.stdout));
-
-    //let output = Command::new("bash")
-    //        .arg("-c")
-    //        .arg(format!("mount | grep {raw_device}"))
-    //        .output()
-    //        .expect("failed to execute process");
-    //println!("## Mount");
-    //println!("{}", String::from_utf8_lossy(&output.stdout));
+    if start_usb {
+        println!("## Init start USB");
+        println!();
+        run_mount(&partition_device_path, &file_system_path);
+        print_mount_status(&raw_device);
+    } else {
+        println!("## Init end USB");
+        println!();
+        println!("Init umount {}", file_system_path);
+        run_command(&format!("sudo umount {file_system_path}"));
+        print_mount_status(&raw_device);
+        println!();
+        println!("Init eject {}", raw_device_path);
+        run_command(&format!("sudo eject {raw_device_path}"));
+        println!();
+        print_devices_status(&raw_device, &suffix_device_path);
+        // https://unix.stackexchange.com/questions/35508/eject-usb-drives-eject-command#83587
+        println!("### Init power off {}", raw_device_path);
+        run_command(&format!("udisksctl power-off -b {raw_device_path}"));
+        print_devices_status(&raw_device, &suffix_device_path);
+    }
 
 }
 
+fn print_devices_status(raw_device: &str, suffix_device_path: &str) {
+    println!("### Devices");
+    run_command(&format!("ls {suffix_device_path} | grep {raw_device}"));
+}
 
 fn print_mount_status(raw_device: &str) {
     println!("### Mount status");
     run_command(&format!("mount | grep {raw_device}"));
 }
 
-fn run_mount(partition_device_path: &str, file_system_path: &str, raw_device: &str){
+// https://linuxconfig.org/howto-mount-usb-drive-in-linux
+fn run_mount(partition_device_path: &str, file_system_path: &str){
     println!("Init mount device {} on {}", partition_device_path, file_system_path);
     run_command(&format!("sudo mount {partition_device_path} {file_system_path}"));
-    print_mount_status(&raw_device);
 }
 
 fn run_command(c: &str) {
