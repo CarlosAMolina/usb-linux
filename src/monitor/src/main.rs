b463882ac1a314e6b17f7d0ee18f468c7083e654
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-03-21 22:50:32 +0100

    Add monitor in rust manage path

--- src/monitor/src/main.rs
+++ src/monitor/src/main.rs
@@ -1,10 +1,20 @@
-use std::fs;
+use std::env;
+use std::process;
+
+use monitor::Config;
+
 
 fn main() {
-    for path in fs::read_dir("/dev").unwrap() {
-        let path_str = path.unwrap().path().display().to_string();
-        if path_str.starts_with("/dev/sd") & path_str.chars().last().unwrap().is_digit(10) {
-            println!("Path: {}", path_str);
-        }
-    }
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|e| {
+        eprintln!("Problem parsing arguments: {}", e);
+        process::exit(1);
+    });
+
+    if let Err(e) = monitor::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    };
 }
+
