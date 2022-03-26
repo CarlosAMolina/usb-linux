b69a3de8a0cf3193b750bb39c6ef3a642698deb7
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-03-26 22:18:57 +0100

    Refactor command_line to file

---
+++ src/rust/src/command_line.rs
@@ -0,0 +1,17 @@
+pub mod command {
+    use std::process::Command;
+
+    pub fn run(c: &str) -> Result<(), String> {
+        println!("Init: {}", c);
+        let output = Command::new("bash")
+            .arg("-c")
+            .arg(c)
+            .output()
+            .expect("failed to execute process");
+        if output.stderr.len() > 0 {
+            return Err(String::from_utf8_lossy(&output.stderr).to_string());
+        }
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+        Ok(())
+    }
+}
--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,22 +1,4 @@
-mod command_line {
-    pub mod command {
-        use std::process::Command;
-
-        pub fn run(c: &str) -> Result<(), String> {
-            println!("Init: {}", c);
-            let output = Command::new("bash")
-                .arg("-c")
-                .arg(c)
-                .output()
-                .expect("failed to execute process");
-            if output.stderr.len() > 0 {
-                return Err(String::from_utf8_lossy(&output.stderr).to_string());
-            }
-            println!("{}", String::from_utf8_lossy(&output.stdout));
-            Ok(())
-        }
-    }
-}
+mod command_line;
 
 use crate::command_line::command;
 
