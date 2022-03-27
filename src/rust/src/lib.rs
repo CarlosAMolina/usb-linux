58e3a321ad53d810cf1a7560465ba79a06ed81de
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-03-27 22:13:25 +0200

    Move monitor in rust

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -173,3 +173,64 @@ mod tests {
         command_line::clear().unwrap();
     }
 }
+
+pub mod monitor {
+    pub fn run(config: Config) -> Result<(), String> {
+        let path = String::from(&config.path);
+        if must_notify_the_path(&path)  {
+            println!("Path: {}", path);
+        }
+        if false {
+            return Err("TODO".to_string());
+        }
+        Ok(())
+    }
+
+    pub struct Config {
+        path: String,
+    }
+
+    impl Config {
+        pub fn new(args: &[String]) -> Result<Config, &'static str> {
+            if args.len() != 2 {
+                help();
+                return Err("not enough arguments");
+            }
+            let path = args[1].clone();
+
+            Ok(Config {
+                path,
+            })
+        }
+    }
+
+    fn help() {
+        eprintln!(
+            "Usage:
+        cargo run <string>
+            Notify a device's path if required.
+    Example:
+        cargo run /dev/sdc1"
+        );
+    }
+
+    fn must_notify_the_path(path: &str) -> bool {
+        return path.starts_with("/dev/sd") & path.chars().last().unwrap().is_digit(10)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn must_notify_the_path_is_true() {
+            assert_eq!(true, must_notify_the_path(&"/dev/sd1".to_string()));
+        }
+
+        #[test]
+        fn must_notify_the_path_is_false() {
+            assert_eq!(false, must_notify_the_path(&"/dev/sd".to_string()));
+        }
+    }
+}
+
--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::process;
 
 use rust::Config;
+use rust::monitor::Config as MonitorConfig;
 
 fn main() {
     if let Err(e) = rust::command_line::clear() {
@@ -11,13 +12,28 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|e| {
-        eprintln!("Problem parsing arguments: {}", e);
-        process::exit(1);
-    });
+    // TODO logic repated in lib.rs
+    if args.len() < 3 {
+        println!("Init monitor");
+        let config = MonitorConfig::new(&args).unwrap_or_else(|e| {
+            eprintln!("Problem parsing arguments: {}", e);
+            process::exit(1);
+        });
 
-    if let Err(e) = rust::run(config) {
-        eprintln!("Application error: {}", e);
-        process::exit(1);
-    };
+        if let Err(e) = rust::monitor::run(config) {
+            eprintln!("Application error: {}", e);
+            process::exit(1);
+        };
+    } else {
+        println!("Init on-off");
+        let config = Config::new(&args).unwrap_or_else(|e| {
+            eprintln!("Problem parsing arguments: {}", e);
+            process::exit(1);
+        });
+
+        if let Err(e) = rust::run(config) {
+            eprintln!("Application error: {}", e);
+            process::exit(1);
+        };
+    }
 }
