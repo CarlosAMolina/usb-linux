3671db3060e704011f2297c9cfcd038c784e088d
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-10-13 00:35:29 +0200

    Add constant for exit status

--- src/usb/src/main.rs
+++ src/usb/src/main.rs
@@ -4,44 +4,47 @@ use std::process;
 use usb::monitor::Config as MonitorConfig;
 use usb::on_off::Config as OnOffConfig;
 
+const OK_EXIT_CODE: i32 = 0;
+const ERROR_EXIT_CODE: i32 = 1;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() == 2 {
         if args[1] == "-h" {
             help();
-            process::exit(0);
+            process::exit(OK_EXIT_CODE);
         } else {
             println!("Init monitor");
             let config = MonitorConfig::new(&args).unwrap_or_else(|e| {
                 eprintln!("Problem parsing arguments: {}", e);
                 help();
-                process::exit(1);
+                process::exit(ERROR_EXIT_CODE);
             });
             if let Err(e) = usb::monitor::run(config) {
                 eprintln!("Application error: {}", e);
-                process::exit(1);
+                process::exit(ERROR_EXIT_CODE);
             };
         }
     } else if args.len() == 3 {
         if let Err(e) = usb::command_line::clear() {
             eprintln!("Problem clear terminal: {}", e);
-            process::exit(1);
+            process::exit(ERROR_EXIT_CODE);
         }
         println!("Init on-off");
         let config = OnOffConfig::new(&args).unwrap_or_else(|e| {
             eprintln!("Problem parsing arguments: {}", e);
             help();
-            process::exit(1);
+            process::exit(ERROR_EXIT_CODE);
         });
         if let Err(e) = usb::on_off::run(config) {
             eprintln!("Application error: {}", e);
-            process::exit(1);
+            process::exit(ERROR_EXIT_CODE);
         };
     } else {
         eprintln!("Problem parsing arguments");
         help();
-        process::exit(1);
+        process::exit(ERROR_EXIT_CODE);
     }
 }
 
