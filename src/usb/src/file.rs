27f2c69448e70c1f8fa6860319220f3c83a7cbbf
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-10-23 01:23:44 +0200

    Refactor extract constant

--- src/usb/src/file.rs
+++ src/usb/src/file.rs
@@ -3,19 +3,19 @@ use std::fs::OpenOptions;
 
 use serde::{Deserialize, Serialize};
 
+const CSV_FILE_PATH_NAME: &str = "/tmp/usb.csv";
+
 pub fn save_mount_info_to_file(device_partition: &String, mounted_path: &String) {
-    let csv_file_path_name = "/tmp/usb.csv".to_string();
-    log::debug!("Init write mount info to {}", csv_file_path_name);
+    log::debug!("Init write mount info to {}", CSV_FILE_PATH_NAME);
     let record = vec![device_partition, mounted_path];
-    append_to_file(&csv_file_path_name, record).unwrap();
+    append_to_file(CSV_FILE_PATH_NAME, record).unwrap();
 }
 
 
 // TODO save headers in save_mount_info_to_file
 pub fn delete_mount_info_in_file(device_partition: &String) {
-    let csv_file_path_name = "/tmp/usb.csv".to_string(); // TODO duplicated
     log::debug!("Init delete mount info of {}", device_partition);
-    let mut rdr = csv::Reader::from_path(&csv_file_path_name).unwrap();
+    let mut rdr = csv::Reader::from_path(CSV_FILE_PATH_NAME).unwrap();
     let mut file_content_vector: Vec<MountInfo> = Vec::new();
     for result in rdr.records() {
         let record = result.unwrap();
@@ -26,11 +26,11 @@ pub fn delete_mount_info_in_file(device_partition: &String) {
         .into_iter()
         .filter(|mount_info| &mount_info.device_partition != device_partition)
         .collect();
-    write_to_new_file(&csv_file_path_name, &new_file_content_vector).unwrap();
+    write_to_new_file(CSV_FILE_PATH_NAME, &new_file_content_vector).unwrap();
     println!("{:?}", new_file_content_vector);
 }
 
-fn append_to_file(file_path: &String, record: Vec<&String>) -> Result<(), Box<dyn Error>> {
+fn append_to_file(file_path: &str, record: Vec<&String>) -> Result<(), Box<dyn Error>> {
     let file = OpenOptions::new()
         .create(true)
         .append(true)
@@ -43,7 +43,7 @@ fn append_to_file(file_path: &String, record: Vec<&String>) -> Result<(), Box<dy
     Ok(())
 }
 
-pub fn write_to_new_file(file_path: &String, records: &Vec<MountInfo>) -> Result<(), Box<dyn Error>> {
+pub fn write_to_new_file(file_path: &str, records: &Vec<MountInfo>) -> Result<(), Box<dyn Error>> {
     let mut wtr = csv::Writer::from_path(file_path)?;
     for record in records {
         wtr.serialize(record)?;
