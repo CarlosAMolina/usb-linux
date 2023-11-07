ff4666ba823283add3a6b4eee35c55de24788416
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-11-07 23:02:50 +0100

    Refactor rename

--- src/usb/src/system.rs
+++ src/usb/src/system.rs
@@ -12,19 +12,19 @@ pub fn show_devices() {
 }
 
 pub fn show_system_current_status(
-    device_raw_path_name_str: &String,
+    device_raw_path_name: &String,
 ) -> command_line::command::CommandResult {
     log::debug!("Init show system current status");
-    let system_status = get_system_current_status(device_raw_path_name_str)?;
+    let system_status = get_system_current_status(device_raw_path_name)?;
     log::debug!("{}", system_status);
     Ok("Ok".to_string())
 }
 
 fn get_system_current_status(
-    device_raw_path_name_str: &String,
+    device_raw_path_name: &String,
 ) -> command_line::command::CommandResult {
-    let devices_status = get_devices_system_current_status(device_raw_path_name_str)?;
-    let mount_status = get_mount_status(device_raw_path_name_str)?;
+    let devices_status = get_devices_system_current_status(device_raw_path_name)?;
+    let mount_status = get_mount_status(device_raw_path_name)?;
     let result = format!(
         "System current status:
 - Connected devices:
@@ -36,11 +36,11 @@ fn get_system_current_status(
 }
 
 fn get_devices_system_current_status(
-    device_raw_path_name_str: &String,
+    device_raw_path_name: &String,
 ) -> command_line::command::CommandResult {
-    let device_raw_path_name = Path::new(device_raw_path_name_str);
-    let devices_path_name = device_raw_path_name.parent().unwrap().to_str().unwrap();
-    let raw_device_name = device_raw_path_name.file_name().unwrap().to_str().unwrap();
+    let device_raw_path = Path::new(device_raw_path_name);
+    let devices_path_name = device_raw_path.parent().unwrap().to_str().unwrap();
+    let raw_device_name = device_raw_path.file_name().unwrap().to_str().unwrap();
     let device_names_str = command_line::command::run(&format!(
         "ls {} | grep {}",
         devices_path_name, raw_device_name
@@ -62,7 +62,23 @@ fn get_devices_system_current_status(
 mod tests {
     use super::*;
 
-    // TODO new test: non existent raw path
+    // TODO #[test]
+    // TODO fn get_system_current_status_if_raw_path_does_not_exist() {
+    // TODO     let device_raw_path_name = "/asdf/foo/asdf/bar".to_string();
+    // TODO     assert_eq!(
+    // TODO         "",
+    // TODO         get_system_current_status(&device_raw_path_name).unwrap()
+    // TODO     );
+    // TODO }
+
+    #[test]
+    fn get_devices_system_current_status_if_raw_path_does_not_exist() {
+        let device_raw_path_name = "/asdf/foo/asdf/bar".to_string();
+        assert_eq!(
+            "",
+            get_devices_system_current_status(&device_raw_path_name).unwrap()
+        );
+    }
 
     #[test]
     fn get_devices_system_current_status_runs_ok_if_no_permissions_on_a_folder() {
